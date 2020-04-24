c2b547b8d96256d1b99b26bc2531791e8e9674fe
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-04-24 22:22:37 +0100

    reform + sandbox.platform fixes

--- modules/azure-private-dns/cname_record.tf
+++ modules/azure-private-dns/cname_record.tf
@@ -5,5 +5,5 @@ resource "azurerm_private_dns_cname_record" "this" {
   zone_name           = azurerm_private_dns_zone.zone.name
   resource_group_name = var.resource_group_name
   ttl                 = each.value.ttl
-  record              = each.value.record
+  record              = lower(each.value.record)
 }
