4155913360c37d3ceb198068a8884c43c18f09ba
luigibk <41112628+luigibk@users.noreply.github.com>
2020-04-24 16:22:40 +0100

    Add sandbox + reform records (#11)
    * Add sandbox + reform records
    
    * Force lowercase names

--- modules/azure-private-dns/a_record.tf
+++ modules/azure-private-dns/a_record.tf
@@ -1,7 +1,7 @@
 resource "azurerm_private_dns_a_record" "this" {
   for_each = { for record in var.a_recordsets : record.name => record }
 
-  name                = each.value.name
+  name                = lower(each.value.name)
   zone_name           = azurerm_private_dns_zone.zone.name
   resource_group_name = var.resource_group_name
   ttl                 = each.value.ttl
--- modules/azure-private-dns/cname_record.tf
+++ modules/azure-private-dns/cname_record.tf
@@ -1,7 +1,7 @@
 resource "azurerm_private_dns_cname_record" "this" {
   for_each = { for record in var.cname_records : record.name => record }
 
-  name                = each.value.name
+  name                = lower(each.value.name)
   zone_name           = azurerm_private_dns_zone.zone.name
   resource_group_name = var.resource_group_name
   ttl                 = each.value.ttl
