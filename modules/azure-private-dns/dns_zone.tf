bc16894f582227790e255a0c3ffe3d4f3de1a1e4
Douglas Brand-Williamson <dougbrw@gmail.com>
2020-04-29 17:05:10 +0100

    added registration_enabled property for vnet links (#23)
    * added registration_enabled property for vnet links
    
    * use correct registration_enabled value
    
    * set registration_enabled to true for idam sprod
    
    Co-authored-by: Henry Dobson <henrydobson@me.com>

--- modules/azure-private-dns/dns_zone.tf
+++ modules/azure-private-dns/dns_zone.tf
@@ -12,4 +12,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.zone.name
   virtual_network_id    = each.value.vnet_id
+  registration_enabled = contains(keys(each.value), "registration_enabled") ? each.value.registration_enabled : false
 }
