7f79dc375e738b492d1dd91c1d04b905a5b4c9ce
Hannah Smith <hannahs@kainos.com>
2020-05-29 09:41:31 +0100

    preview dns zone

--- components/preview/data.tf
+++ components/preview/data.tf
@@ -6,3 +6,6 @@ data "local_file" "preview-idam-internal-configuration" {
   filename = "${path.cwd}/../../environments/preview/service-core-compute-idam-preview-internal.yml"
 }
 
+data "local_file" "preview-platform-configuration" {
+  filename = "${path.cwd}/../../environments/preview/preview-platform-hmcts-net.yml}"
+}
---
+++ components/preview/preview-platform.tf
@@ -0,0 +1,9 @@
+module "preview-platform" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.preview-platform-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.preview-platform-configuration.content).A
+  zone_name           = yamldecode(data.local_file.preview-platform-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.preview-platform-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
