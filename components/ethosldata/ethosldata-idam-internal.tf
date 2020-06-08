708a80195f06980e045e6a4e0c74212ef3086450
Isher Singh Raikmo <35302598+isherraikmo@users.noreply.github.com>
2020-06-08 15:11:16 +0100

    ethosldata

---
+++ components/ethosldata/ethosldata-idam-internal.tf
@@ -0,0 +1,13 @@
+data "local_file" "ethosldata-idam-internal-configuration" {
+  filename = "${path.cwd}/../../environments/ethosldata/service-core-compute-idam-ethosldata-internal.yml"
+}
+
+module "sprod-idam-internal" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.sprod-idam-internal-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.sprod-idam-internal-configuration.content).A
+  zone_name           = yamldecode(data.local_file.sprod-idam-internal-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.sprod-idam-internal-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
