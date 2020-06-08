0ccfc98a7978cfb95815ac6a3b3a8f249c4eda73
isherraikmo <35302598+isherraikmo@users.noreply.github.com>
2020-06-08 16:44:30 +0100

    ethosldata (#46)
    * ethosldata

---
+++ components/ethosldata/data.tf
@@ -0,0 +1,7 @@
+data "local_file" "ethosldata-internal-configuration" {
+  filename = "${path.cwd}/../../environments/ethosldata/service-core-compute-ethosldata-internal.yml"
+}
+
+data "local_file" "ethosldata-idam-internal-configuration" {
+  filename = "${path.cwd}/../../environments/ethosldata/service-core-compute-idam-ethosldata-internal.yml"
+}
