170e09dac8fd858d8b35c7b38761005597abe635
Isher Singh Raikmo <35302598+isherraikmo@users.noreply.github.com>
2020-06-22 13:31:29 +0100

    added yaml loading

--- components/ethosldata/data.tf
+++ components/ethosldata/data.tf
@@ -5,3 +5,7 @@ data "local_file" "ethosldata-internal-configuration" {
 data "local_file" "ethosldata-idam-internal-configuration" {
   filename = "${path.cwd}/../../environments/ethosldata/service-core-compute-idam-ethosldata-internal.yml"
 }
+
+data "local_file" "ethosldata-platform-configuration" {
+  filename = "${path.cwd}/../../environments/ethosldata/ethosldata-platform-hmcts-net.yml"
+}
