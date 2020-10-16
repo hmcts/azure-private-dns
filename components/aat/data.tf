1503127d80c483f5950f5b8bb7fc479bdcda2e2b
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-16 11:28:08 +0100

    Fix paths

--- components/aat/data.tf
+++ components/aat/data.tf
@@ -1,15 +1,15 @@
 data "local_file" "aat-internal-configuration" {
-  filename = "${path.cwd}/../../environments/aat/service-core-compute-aat-internal.yml"
+  filename = "${path.cwd}/../../environments/staging/service-core-compute-aat-internal.yml"
 }
 
 data "local_file" "aat-idam-internal-configuration" {
-  filename = "${path.cwd}/../../environments/aat/service-core-compute-idam-aat-internal.yml"
+  filename = "${path.cwd}/../../environments/staging/service-core-compute-idam-aat-internal.yml"
 }
 
 data "local_file" "aat2-idam-internal-configuration" {
-  filename = "${path.cwd}/../../environments/aat/service-core-compute-idam-aat2-internal.yml"
+  filename = "${path.cwd}/../../environments/staging/service-core-compute-idam-aat2-internal.yml"
 }
 
 data "local_file" "aat-platform-configuration" {
-  filename = "${path.cwd}/../../environments/aat/aat-platform-hmcts-net.yml"
+  filename = "${path.cwd}/../../environments/staging/aat-platform-hmcts-net.yml"
 }
--- components/preview/data.tf
+++ components/preview/data.tf
@@ -1,11 +1,11 @@
 data "local_file" "preview-internal-configuration" {
-  filename = "${path.cwd}/../../environments/preview/service-core-compute-preview-internal.yml"
+  filename = "${path.cwd}/../../environments/dev/service-core-compute-preview-internal.yml"
 }
 
 data "local_file" "preview-idam-internal-configuration" {
-  filename = "${path.cwd}/../../environments/preview/service-core-compute-idam-preview-internal.yml"
+  filename = "${path.cwd}/../../environments/dev/service-core-compute-idam-preview-internal.yml"
 }
 
 data "local_file" "preview-platform-configuration" {
-  filename = "${path.cwd}/../../environments/preview/preview-platform-hmcts-net.yml"
+  filename = "${path.cwd}/../../environments/dev/preview-platform-hmcts-net.yml"
 }
