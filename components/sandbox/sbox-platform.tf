479aa4f206fc725ac99e2143d8db9bd280b6aa84
James Matthews <stravze@gmail.com>
2021-02-01 17:06:49 +0000

    adding sbox zone file to align with environment names and moved vh-us… (#178)
    * adding sbox zone file to align with environment names and moved vh-user-api
    
    * fixing file pathing
    
    * file extension incorrect
    
    * missed cname: []

---
+++ components/sandbox/sbox-platform.tf
@@ -0,0 +1,13 @@
+data "local_file" "sbox-platform-configuration" {
+  filename = "${path.cwd}/../../environments/sandbox/sbox-platform-hmcts-net.yml"
+}
+
+module "sbox-platform" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.sbox-platform-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.sbox-platform-configuration.content).A
+  zone_name           = yamldecode(data.local_file.sbox-platform-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.sbox-platform-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
