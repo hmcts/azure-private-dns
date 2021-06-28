86552d199780c54ec7e3719859db44bd7c3d1556
Luigi Bitonti <luigi.bitonti@hmcts.net>
2021-06-28 13:51:18 +0100

    DTSPO-3268 add mailrelay zone

---
+++ components/prod/mailrelay.tf
@@ -0,0 +1,13 @@
+data "local_file" "mailrelay-configuration" {
+  filename = "${path.cwd}/../../environments/prod/mailrelay-platform-hmcts-net.yml"
+}
+
+module "mailrelay-internal" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.mailrelay-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.mailrelay-configuration.content).A
+  zone_name           = yamldecode(data.local_file.mailrelay-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.mailrelay-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
