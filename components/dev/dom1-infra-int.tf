5dd85c079c8868f16e66f31dd7b314403198f416
Zoë <54357991+zc-hmcts@users.noreply.github.com>
2021-07-20 09:33:14 +0100

    DTSPO-3575 Add New DNS Zone and Entries for dom1.infra.int (#259)
    * DTSPO-3575 Add New DNS Zone and Entries for dom1.infra.int
    
    * Update environments/dev/dom1-infra-int.yml
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

---
+++ components/dev/dom1-infra-int.tf
@@ -0,0 +1,13 @@
+data "local_file" "dom1-infra-int-configuration" {
+  filename = "${path.cwd}/../../environments/dev/dom1-infra-int.yml"
+}
+
+module "dom1-infra-int" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.dom1-infra-int-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.dom1-infra-int-configuration.content).A
+  zone_name           = yamldecode(data.local_file.dom1-infra-int-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.dom1-infra-int-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
