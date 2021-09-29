97b7391c902fd21eaa2b619fe4183c5a03eaeba9
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-09-29 16:08:23 +0100

    DTSPO-5038 Provide option to not create zone (#295)

--- modules/azure-private-dns/dns_zone.tf
+++ modules/azure-private-dns/dns_zone.tf
@@ -2,7 +2,12 @@ resource "azurerm_private_dns_zone" "zone" {
   name                = var.zone_name
   resource_group_name = var.resource_group_name
 
-  tags = module.ctags.common_tags
+  tags  = module.ctags.common_tags
+  count = var.create_zone == true ? 1 : 0
+}
+
+locals {
+  zone = var.create_zone == true ? azurerm_private_dns_zone.zone[0].name : var.zone_name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
@@ -10,9 +15,9 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
 
   name                  = each.value.link_name
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.zone.name
+  private_dns_zone_name = local.zone
   virtual_network_id    = each.value.vnet_id
-  registration_enabled = contains(keys(each.value), "registration_enabled") ? each.value.registration_enabled : false
+  registration_enabled  = contains(keys(each.value), "registration_enabled") ? each.value.registration_enabled : false
 }
 
 module "ctags" {
