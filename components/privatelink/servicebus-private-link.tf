7e84217a96811d5170a9c90250769e23fee794b1
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-10 13:32:44 +0000

    servicebub

---
+++ components/privatelink/servicebus-private-link.tf
@@ -0,0 +1,13 @@
+data "local_file" "servicebus-private-link" {
+  filename = "${path.cwd}/../../environments/privatelink/servicebus-private-link.yml"
+}
+
+module "postgres-private-link" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.servicebus-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.servicebus-private-link.content).A
+  zone_name           = yamldecode(data.local_file.servicebus-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.servicebus-private-link.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
