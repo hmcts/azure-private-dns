d635e95f12a515c232ecdd4570300136c5ecc62e
thomast1906 <thomast@kainos.com>
2022-02-11 10:15:21 +0000

    Add private dns zones for redis and signalr

---
+++ components/privatelink/signalr-private-link.tf
@@ -0,0 +1,13 @@
+data "local_file" "blob-private-link" {
+  filename = "${path.cwd}/../../environments/privatelink/signalr-private-link.yml"
+}
+
+module "blob-private-link" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.blob-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.blob-private-link.content).A
+  zone_name           = yamldecode(data.local_file.blob-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.blob-private-link.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
