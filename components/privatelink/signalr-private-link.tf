e6f9eac519a245fe1c7dea1e4b153e86d737e4f7
Chris C Moynihan <chriscm.neu@gmail.com>
2022-02-14 11:46:09 +0000

    Add zones redis signalr (#336)
    * Add private dns zones for redis and signalr
    
    * added sharedservice non-prod vnet links
    
    * added ss vnet peering for signalr
    
    * fixing names
    
    * removed SDS vNet link, will happen in aks-sds-deploy repo
    
    Co-authored-by: thomast1906 <thomast@kainos.com>

---
+++ components/privatelink/signalr-private-link.tf
@@ -0,0 +1,13 @@
+data "local_file" "signalr-private-link" {
+  filename = "${path.cwd}/../../environments/privatelink/signalr-private-link.yml"
+}
+
+module "signalr-private-link" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.signalr-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.signalr-private-link.content).A
+  zone_name           = yamldecode(data.local_file.signalr-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.signalr-private-link.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
