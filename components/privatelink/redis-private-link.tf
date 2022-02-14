a2b05f946820b21ac0e565d41af6e82148c7be12
lowflying <chriscm.neu@gmail.com>
2022-02-14 11:03:53 +0000

    fixing names

--- components/privatelink/redis-private-link.tf
+++ components/privatelink/redis-private-link.tf
@@ -1,13 +1,13 @@
-data "local_file" "blob-private-link" {
+data "local_file" "redis-private-link" {
   filename = "${path.cwd}/../../environments/privatelink/redis-private-link.yml"
 }
 
-module "blob-private-link" {
+module "redis-private-link" {
   source              = "../../modules/azure-private-dns/"
-  cname_records       = yamldecode(data.local_file.blob-private-link.content).cname
-  a_recordsets        = yamldecode(data.local_file.blob-private-link.content).A
-  zone_name           = yamldecode(data.local_file.blob-private-link.content).name
-  vnet_links          = yamldecode(data.local_file.blob-private-link.content).vnet_links
+  cname_records       = yamldecode(data.local_file.redis-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.redis-private-link.content).A
+  zone_name           = yamldecode(data.local_file.redis-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.redis-private-link.content).vnet_links
   resource_group_name = var.resource_group_name
   env                 = var.env
 }
