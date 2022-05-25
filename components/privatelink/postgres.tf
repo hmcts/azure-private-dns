93b0d9b23bdd62fd4f6011a6aee425bf2ce80969
Tim Jacomb <tim.jacomb@hmcts.net>
2022-05-25 11:05:54 +0100

    Add postgres private dns zone

---
+++ components/privatelink/postgres.tf
@@ -0,0 +1,13 @@
+data "local_file" "postgres" {
+  filename = "${path.cwd}/../../environments/privatelink/postgres.yml"
+}
+
+module "postgres" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.postgres.content).cname
+  a_recordsets        = yamldecode(data.local_file.postgres.content).A
+  zone_name           = yamldecode(data.local_file.postgres.content).name
+  vnet_links          = yamldecode(data.local_file.postgres.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+}
