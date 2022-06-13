0d1e013ba679366de89009f3e9ab64d75928fe1d
Zoë <54357991+zc-hmcts@users.noreply.github.com>
2022-06-13 08:47:47 +0100

    Revert "Update Terraform azurerm to v2.99.0" (#387)

--- components/sandbox/provider.tf
+++ components/sandbox/provider.tf
@@ -1,14 +1,8 @@
 provider "azurerm" {
-  version = "=2.99.0"
+  version = "=2.3.0"
   features {}
-  skip_provider_registration = true
 }
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.99.0"
-    }
-  }
+  backend "azurerm" {}
 }
