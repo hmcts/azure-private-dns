f8926c46c301993d817cb2cb2ff29f450eeb8d63
Zoe Cope <zoe.cope@hmcts.net>
2022-06-15 13:21:06 +0100

    testing

--- components/sandbox/provider.tf
+++ components/sandbox/provider.tf
@@ -6,8 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.10.0"
-      backend "azurerm" {}
+      version = "=3.10.0"
     }
   }
 }
