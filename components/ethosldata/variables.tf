f13301bbf0419b0f0aa2859f8920afeb5197618d
Tim Jacomb <tim.jacomb@hmcts.net>
2022-06-17 10:58:40 +0100

    Remove ethosldata (subscription deleted ~1 year ago)

--- components/ethosldata/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group that contains the DNS zones."
-  default     = "core-infra-intsvc-rg"
-}
-
-variable "env" {
-  default = "ldata"
-}
