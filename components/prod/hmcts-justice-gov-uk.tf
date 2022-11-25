b6408dcb6775386c8c6b6147a649bd27b095984c
Alex Bance <Alex.Bance@justice.gov.uk>
2022-11-25 09:25:17 +0000

    fix: undeclared reference

--- components/prod/hmcts-justice-gov-uk.tf
+++ components/prod/hmcts-justice-gov-uk.tf
@@ -4,10 +4,10 @@ data "local_file" "justice-configuration" {
 
 module "hmcts-justice-gov-uk" {
   source              = "../../modules/azure-private-dns/"
-  cname_records       = yamldecode(data.local_file.hmcts-justice-gov-uk.content).cname
-  a_recordsets        = yamldecode(data.local_file.hmcts-justice-gov-uk.content).A
-  zone_name           = yamldecode(data.local_file.hmcts-justice-gov-uk.content).name
-  vnet_links          = yamldecode(data.local_file.hmcts-justice-gov-uk.content).vnet_links
+  cname_records       = yamldecode(data.local_file.justice-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.justice-configuration.content).A
+  zone_name           = yamldecode(data.local_file.justice-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.justice-configuration.content).vnet_links
   resource_group_name = var.resource_group_name
   env                 = var.env
   builtFrom           = var.builtFrom
