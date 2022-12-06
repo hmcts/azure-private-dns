5573e41066d1b9294c6f035ebff225258218f185
Alex Bance <Alex.Bance@justice.gov.uk>
2022-12-06 12:30:38 +0000

    fix: naming consistency

--- components/test/test-internal-hmcts-net.tf
+++ components/test/test-internal-hmcts-net.tf
@@ -1,13 +1,13 @@
-data "local_file" "test-internal-hmcts-net-configuration" {
+data "local_file" "test-internal-hmcts-configuration" {
   filename = "${path.cwd}/../../environments/test/test-internal-hmcts-net.yml"
 }
 
 module "test-internal-hmcts-net" {
   source              = "../../modules/azure-private-dns/"
-  cname_records       = yamldecode(data.local_file.test-internal-hmcts-net-configuration.content).cname
-  a_recordsets        = yamldecode(data.local_file.test-internal-hmcts-net-configuration.content).A
-  zone_name           = yamldecode(data.local_file.test-internal-hmcts-net-configuration.content).name
-  vnet_links          = yamldecode(data.local_file.test-internal-hmcts-net-configuration.content).vnet_links
+  cname_records       = yamldecode(data.local_file.test-internal-hmcts-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.test-internal-hmcts-configuration.content).A
+  zone_name           = yamldecode(data.local_file.test-internal-hmcts-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.test-internal-hmcts-configuration.content).vnet_links
   resource_group_name = var.resource_group_name
   env                 = var.env
   builtFrom           = var.builtFrom
