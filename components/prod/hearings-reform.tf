1998192b729216bd372036f21786f53d6ebfc127
Will Watters <will.watters1@justice.gov.uk>
2023-02-27 16:53:25 +0000

    DTSPO-12579 - Creating private dns zone - hearings-reform-hmcts-net

---
+++ components/prod/hearings-reform.tf
@@ -0,0 +1,15 @@
+data "local_file" "hearings-reform-configuration" {
+  filename = "${path.cwd}/../../environments/prod/hearings-reform-hmcts-net.yml"
+}
+
+module "reform" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.hearings-reform-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.hearings-reform-configuration.content).A
+  zone_name           = yamldecode(data.local_file.hearings-reform-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.hearings-reform-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  builtFrom           = var.builtFrom
+  product             = var.product
+}
