501c92ca8cf75e7e6d1d4d3323db60e27ce8969e
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-06-01 14:45:24 +0100

    added nle int and ext private dns zones (#582)
    * added nle int and ext private dns zones
    
    * fixed file name issue
    
    * testing pester module fix
    
    * udpated back to master

---
+++ components/staging/nle-int.tf
@@ -0,0 +1,15 @@
+data "local_file" "nle-int-hmcts-configuration" {
+  filename = "${path.cwd}/../../environments/staging/nle-int-platform-hmcts-net.yml"
+}
+
+module "nle-int-hmcts-net" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.nle-int-hmcts-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.nle-int-hmcts-configuration.content).A
+  zone_name           = yamldecode(data.local_file.nle-int-hmcts-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.nle-int-hmcts-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  builtFrom           = var.builtFrom
+  product             = var.product
+}
