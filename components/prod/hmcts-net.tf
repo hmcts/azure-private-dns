ee0cf8451195a871d5499069505c9540f18636c3
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-06-06 12:30:41 +0100

    added hmcts net private dns zone (#586)
    * added hmcts net private dns zone
    
    * updated module name

---
+++ components/prod/hmcts-net.tf
@@ -0,0 +1,15 @@
+data "local_file" "hmcts-net-configuration" {
+  filename = "${path.cwd}/../../environments/prod/hmcts-net.yml"
+}
+
+module "hmcts-net" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.hmcts-net-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.hmcts-net-configuration.content).A
+  zone_name           = yamldecode(data.local_file.hmcts-net-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.hmcts-net-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  builtFrom           = var.builtFrom
+  product             = var.product
+}
