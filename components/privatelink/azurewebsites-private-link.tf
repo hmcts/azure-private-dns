5d4010ff50cd85b5f14232510b9847575af1e844
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-03 11:15:12 +0100

    update zone in azure websites link

--- components/privatelink/azurewebsites-private-link.tf
+++ components/privatelink/azurewebsites-private-link.tf
@@ -4,10 +4,10 @@ data "local_file" "azurewebsites-private-link" {
 
 module "azurewebsites-private-link" {
   source              = "../../modules/azure-private-dns/"
-  cname_records       = yamldecode(data.local_file.blob-private-link.content).cname
-  a_recordsets        = yamldecode(data.local_file.blob-private-link.content).A
-  zone_name           = yamldecode(data.local_file.blob-private-link.content).name
-  vnet_links          = yamldecode(data.local_file.blob-private-link.content).vnet_links
+  cname_records       = yamldecode(data.local_file.azurewebsites-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.azurewebsites-private-link.content).A
+  zone_name           = yamldecode(data.local_file.azurewebsites-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.azurewebsites-private-link.content).vnet_links
   resource_group_name = var.resource_group_name
   env                 = var.env
   builtFrom           = var.builtFrom
