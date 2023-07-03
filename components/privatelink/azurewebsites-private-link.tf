28cdfbf33321589f62b88ab600aba6e079461a32
Tony Chow <36265570+tchow8@users.noreply.github.com>
2023-07-03 09:38:51 +0100

    Add azure websites private dns zone (#601)

---
+++ components/privatelink/azurewebsites-private-link.tf
@@ -0,0 +1,15 @@
+data "local_file" "azurewebsites-private-link" {
+  filename = "${path.cwd}/../../environments/privatelink/azurewebsites-private-link.yml"
+}
+
+module "azurewebsites-private-link" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.blob-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.blob-private-link.content).A
+  zone_name           = yamldecode(data.local_file.blob-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.blob-private-link.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  builtFrom           = var.builtFrom
+  product             = var.product
+}
