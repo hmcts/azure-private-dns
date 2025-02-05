9f0fb7a48788600d560af8b5f62f82312f3da14c
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-02-05 11:51:44 +0000

    Added provider.tf to perftest component

---
+++ components/perftest/provider.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.81.0"
+    }
+  }
+}
