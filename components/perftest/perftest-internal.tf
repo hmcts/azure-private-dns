3abd9bba9e80bc91f10d986b1dfdd9e25b7cafad
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-02-05 11:08:37 +0000

    Created perftest component so that perftest can then be added to azure-pipelines.yml next

---
+++ components/perftest/perftest-internal.tf
@@ -0,0 +1,15 @@
+data "local_file" "perftest-internal-configuration" {
+  filename = "${path.cwd}/../../environments/perftest/service-core-compute-perftest-internal.yml"
+}
+
+module "ithc-internal" {
+  source              = "../../modules/azure-private-dns/"
+  cname_records       = yamldecode(data.local_file.perftest-internal-configuration.content).cname
+  a_recordsets        = yamldecode(data.local_file.perftest-internal-configuration.content).A
+  zone_name           = yamldecode(data.local_file.perftest-internal-configuration.content).name
+  vnet_links          = yamldecode(data.local_file.perftest-internal-configuration.content).vnet_links
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  builtFrom           = var.builtFrom
+  product             = var.product
+}
