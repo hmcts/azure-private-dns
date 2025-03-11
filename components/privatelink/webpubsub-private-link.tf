deec129abc75017217a54d4a226ced0fc46f421f
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-03-11 11:56:08 +0000

    fixing typo

--- components/privatelink/webpubsub-private-link.tf
+++ components/privatelink/webpubsub-private-link.tf
@@ -2,7 +2,7 @@ data "local_file" "webpubsub-private-link" {
   filename = "${path.cwd}/../../environments/privatelink/webpubsub-private-link.yml"
 }
 
-module "vault-private-link" {
+module "webpubsub-private-link" {
   source              = "../../modules/azure-private-dns/"
   cname_records       = yamldecode(data.local_file.webpubsub-private-link.content).cname
   a_recordsets        = yamldecode(data.local_file.webpubsub-private-link.content).A
