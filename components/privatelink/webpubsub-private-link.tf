15874e6dd6aa0d95c4243bed2b334824c04aca9e
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-03-11 11:50:40 +0000

    fixing typo

--- components/privatelink/webpubsub-private-link.tf
+++ components/privatelink/webpubsub-private-link.tf
@@ -1,13 +1,13 @@
-data "local_file" "vault-private-link" {
+data "local_file" "webpubsub-private-link" {
   filename = "${path.cwd}/../../environments/privatelink/webpubsub-private-link.yml"
 }
 
 module "vault-private-link" {
   source              = "../../modules/azure-private-dns/"
-  cname_records       = yamldecode(data.local_file.vault-private-link.content).cname
-  a_recordsets        = yamldecode(data.local_file.vault-private-link.content).A
-  zone_name           = yamldecode(data.local_file.vault-private-link.content).name
-  vnet_links          = yamldecode(data.local_file.vault-private-link.content).vnet_links
+  cname_records       = yamldecode(data.local_file.webpubsub-private-link.content).cname
+  a_recordsets        = yamldecode(data.local_file.webpubsub-private-link.content).A
+  zone_name           = yamldecode(data.local_file.webpubsub-private-link.content).name
+  vnet_links          = yamldecode(data.local_file.webpubsub-private-link.content).vnet_links
   resource_group_name = var.resource_group_name
   env                 = var.env
 }
