03922394184a43d9c5ae08a6d5c0e5bb1be6ef98
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2025-11-19 16:20:27 +0000

    Add plum-recipe-backend-tls DNS record for APIM TLS testing

--- components/sandbox/sandbox.tf
+++ components/sandbox/sandbox.tf
@@ -1,5 +1,5 @@
-resource "azurerm_private_dns_a_record" "plum_recipe_backend" {
-  name                = "plum-recipe-backend"
+resource "azurerm_private_dns_a_record" "plum_recipe_backend_tls" {
+  name                = "plum-recipe-backend-tls"
   zone_name           = "sandbox.platform.hmcts.net"
   resource_group_name = var.resource_group_name
   ttl                 = 300
