7aaea4d6104e8db4ecb6173f9cded3bdffb71449
Daniel Kiriasidis Christou <55547489+DanielKChristou@users.noreply.github.com>
2025-11-20 15:52:31 +0000

    Correct IP to point to the correct app gateway (#961)

--- components/sandbox/sandbox.tf
+++ components/sandbox/sandbox.tf
@@ -3,5 +3,5 @@ resource "azurerm_private_dns_a_record" "plum_recipe_backend_tls" {
   zone_name           = "sandbox.platform.hmcts.net"
   resource_group_name = var.resource_group_name
   ttl                 = 300
-  records             = ["20.108.136.254"]
+  records             = ["51.104.255.196"]
 }
