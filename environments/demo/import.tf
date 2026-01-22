8aec3549f57d8cc03da3c6b8157f7f9a8cd61622
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2026-01-22 12:45:47 +0000

    add import block for demo

---
+++ environments/demo/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.demo-apps-private-dns.azurerm_private_dns_cname_record.this["juror-er"]
+  id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/demo.apps.hmcts.net/CNAME/juror-er"
+}
