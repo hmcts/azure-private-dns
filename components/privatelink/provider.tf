c12cab167fe6af353bfb3e62c00e3c8f61eb3093
Daniel Kiriasidis Christou <55547489+DanielKChristou@users.noreply.github.com>
2026-04-14 13:49:55 +0100

    DTSPO-30425: Add crime-idam VNet links to postgres privatelink DNS zone (#998)
    * DTSPO-30425: Add crime-idam VNet links to postgres privatelink DNS zone
    
    * DTSPO-30425: implement cross-tenant postgres DNS links via aliased provider and separate module path
    
    * fix: use non-live tenant and VNets for cross-tenant DNS links
    
    * Rename TFvars

--- components/privatelink/provider.tf
+++ components/privatelink/provider.tf
@@ -3,6 +3,19 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
+provider "azurerm" {
+  alias = "cross_tenant_nonlive"
+
+  features {}
+  resource_provider_registrations = "none"
+
+  subscription_id      = var.cross_tenant_nonlive_subscription_id
+  tenant_id            = var.cross_tenant_nonlive_tenant_id
+  client_id            = var.cross_tenant_nonlive_client_secret != "" ? var.cross_tenant_nonlive_client_id : null
+  client_secret        = var.cross_tenant_nonlive_client_secret != "" ? var.cross_tenant_nonlive_client_secret : null
+  auxiliary_tenant_ids = var.cross_tenant_nonlive_auxiliary_tenant_ids
+}
+
 terraform {
   required_version = ">= 1.8.0"
 
