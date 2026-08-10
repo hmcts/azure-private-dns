f2d5d6804a94c339c64ea1e07ea45615c42aa693
David Izuogu <david.izuogu@justice.gov.uk>
2026-08-10 15:50:02 +0100

    remove import file
    Signed-off-by: David Izuogu <david.izuogu@justice.gov.uk>

--- components/demo/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.demo-platform.azurerm_private_dns_a_record.this["em-icp-webpubsub"]
-  id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/demo.platform.hmcts.net/A/em-icp-webpubsub"
-}
